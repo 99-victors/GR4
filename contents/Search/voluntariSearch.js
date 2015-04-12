12ed07b0653e9becf8f7b70e14dda66c6127fc3f
Victor <lopezv@mit.edu>
2015-04-12 13:54:19 -0400

    added preliminary search page code

---
+++ contents/Search/voluntariSearch.js
@@ -0,0 +1,41 @@
+// This allows the Javascript code inside this block to only run when the page
+// has finished loading in the browser.
+$(function() {
+
+//This script extracts parameters from the URL
+//from jquery-howto.blogspot.com
+
+    $.extend({
+        getUrlVars : function() {
+            var vars = [], hash;
+            var hashes = window.location.href.slice(
+                    window.location.href.indexOf('?') + 1).split('&');
+            for ( var i = 0; i < hashes.length; i++) {
+                hash = hashes[i].split('=');
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+            return vars;
+        },
+        getUrlVar : function(name) {
+            return $.getUrlVars()[name];
+        }
+    });
+    
+    console.log("lol");
+    
+        
+    var searchQuery = "";
+        
+    if($.getUrlVar('query')){
+        searchQuery = $.getUrlVar('query');
+    }
+    
+    $("#test").html(searchQuery);
+    
+    
+    
+    
+    
+    
+    });
