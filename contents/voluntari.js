530b878163c34069c6e7738d5325076faed27957
Shi-Ke Xue <shikexue@mit.edu>
2015-04-13 19:45:59 -0400

    add botstrap and start messing with the search bar

--- contents/voluntari.js
+++ contents/voluntari.js
@@ -1,9 +1,11 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
-$(function() {
-
+$(document).ready(function() {
     $('#searchButton').click(function(){
         $("#content").prop('data','Search/voluntariSearch.html?query=' + $('#searchBox').val());
     });
 
+    $('#advancedSearch').click(function() {
+    	
+    });
 });
