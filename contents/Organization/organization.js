5a2f680149591317c83e4697ce0159b00cf11464
Shi-Ke Xue <shikexue@mit.edu>
2015-05-04 06:47:07 -0400

    create event abstraction for organizations, allow add event to do modify organizaiton page (although currently only adds canned 'a' strings

--- contents/Organization/organization.js
+++ contents/Organization/organization.js
@@ -11,8 +11,19 @@ $(document).ready(function() {
 
     $('#add-form').load('add.html', function() {
     	$('#datetimepicker').datetimepicker();
+    	$('#add-event-button').click(function(e) {
+    		if (confirm('Are you sure you want to add this event?')) {
+    			var event = new Event({
+    				name: 'a',
+    				time: 'a', 
+    				desc: 'a',
+    				skills: 'a',
+    			});
+    			$('#events-tab').append(event.toHTML());
+    		}
+    	});
     });
 
-    var mapOptions = {zoom: 8, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};    
-    var map = new google.maps.Map(mapDiv, mapOptions);
+    //var mapOptions = {zoom: 8, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};    
+    //var map = new google.maps.Map(mapDiv, mapOptions);
 });
