b2556ecc59d6b09bfbfdc78aa63efa80377b870c
Victor <lopezv@mit.edu>
2015-05-04 04:10:01 -0400

    preliminary profile page

--- contents/Event/result.js
+++ contents/Event/result.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#navbar').load('../partials/navbar.html', function() {
 		$('#navbar-home').attr('href', '../voluntari.html');
+      	$('#navbar-prof').attr('href', '../profile/profile.html');      
 	});
 
 	$('#submitButton').click(function(){
--- contents/Organization/organization.js
+++ contents/Organization/organization.js
@@ -2,18 +2,8 @@ $(document).ready(function() {
     //$('#datepicker').datepicker({});
     $('#navbar').load('../partials/navbar.html', function() {
     	$('#navbar-home').attr('href', '../voluntari.html');
+      	$('#navbar-prof').attr('href', '../profile/profile.html');
+
     });
     
-
-    $('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = $(this).attr('href');
- 
-        // Show/Hide Tabs
-        $('.tabs ' + currentAttrValue).show().siblings().hide();
- 
-        // Change/remove current tab to active
-        $(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
-    });
 });
