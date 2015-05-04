8e1167995d05a5b2ccc35c6005b5bdc8582a809c
Shi-Ke Xue <shikexue@mit.edu>
2015-05-04 06:11:28 -0400

    add datetimepicker package and have it working in organization page

--- contents/Organization/organization.js
+++ contents/Organization/organization.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    //$('#datepicker').datepicker({});
     $('#navbar').load('../partials/navbar.html', function() {
     	$('#navbar-brand').attr('href', '../voluntari.html');
     	$('#navbar-home').attr('href', '../voluntari.html');
       	$('#navbar-prof').attr('href', '../profile/profile.html');
       	$('#navbar-logout').attr('href', '../homepage.html');
     });
+
+	$('#datetimepicker').datetimepicker();
+
     var mapOptions = {zoom: 8, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};    
     var map = new google.maps.Map(mapDiv, mapOptions);
 });
