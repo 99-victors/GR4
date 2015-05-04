705de9dd19237f9a2de8953c4e75bacdc02506b4
Shi-Ke Xue <shikexue@mit.edu>
2015-05-04 08:36:13 -0400

    create event class to generate html

--- contents/voluntari.js
+++ contents/voluntari.js
@@ -10,6 +10,35 @@ $(document).ready(function () {
         $('#navbar-logout').attr('href', './homepage.html');
     }); 
 
+    $('#results').append(new Event({
+        desc: 'Learn how to fix up bikes.',
+        title: 'Mechanics Training',
+        time: 'Sun: 1pm - 2pm',
+        org: 'Bikes Not Bombs',
+        tele: '555-555-5555',
+        address: '18 Bartlett Sq. Jamaica Plain, MA 02130',
+        skills: 'None'
+    }).toHTML());
+
+    $('#results').append(new Event({
+        desc: 'Pack up books in boxes to be shipped to prisons.',
+        title: 'Book Packing',
+        time: 'Sun: 3pm - 8pm',
+        org: 'Prison Books',
+        tele: '555-555-5556', 
+        address: '1306 Hancock St. Quincy, MA 02169',
+        skills: 'None'
+    }).toHTML());
+
+    $('#results').append(new Event({
+        desc: 'Help act as waiters for our guests and serve food.',
+        title: 'Serving Food',
+        time: 'Sun: 3pm - 9pm',
+        org: 'Rosie\'s Place',
+        tele: '555-555-5557',
+        address: '889 Harisson Ave. Boston, MA 02118',
+        skills: 'None'
+    }).toHTML());  
 
     var performSearch = function () {
         //$('#content').prop('data', 'Search/voluntariSearch.html?query=' + $('#searchBox').val());
