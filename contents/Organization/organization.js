26bdf3dfa120ca3f7f26b826837b283c18feaffa
Victor <lopezv@mit.edu>
2015-05-04 11:44:31 -0400

    merged

--- contents/Organization/organization.js
+++ contents/Organization/organization.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       	$('#navbar-logout').attr('href', '../homepage.html');
     });
 
-/*  
+  
     function getCookie(cname) {
         var name = cname + "=";
         var ca = document.cookie.split(';');
@@ -42,6 +42,20 @@ $(document).ready(function() {
 	    		}
 	    	});
 	    });
+      
+      $('#edit-form').load('edit.html', function(){
+        $('#edit-button').click(function(e){
+ 		if (confirm('Are you sure you want to update this information?')) {          
+          $('#mission').html($('#miss').val())
+          $('#info').html($('#mail').val())
+          $('#tele').html($('#phone').val())
+          $('#page').html($('#site').val()) 
+      	  $('#page').attr('href', $('#site').val());
+	      $('#edit-form').modal('hide');
+          
+        }
+        });
+      });
     });
 
     /*var geocoder = new google.maps.Geocoder();
@@ -67,7 +81,7 @@ $(document).ready(function() {
 		 	//google.maps.event.trigger(map, 'resize');
 		});
     });
-*
+
 
 
 
--- contents/profile/profile.js
+++ contents/profile/profile.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
     //$('#datepicker').datepicker({});
     $('#navbar').load('../partials/navbar.html', function() {
-    	$('#navbar-home').attr('href', '../voluntari.html');
+    	$('#navbar-brand').attr('href', '../voluntari.html');
+
+      $('#navbar-home').attr('href', '../voluntari.html');
     	$('#navbar-prof').attr('href', 'profile.html');
+        $('#navbar-logout').attr('href', '../homepage.html');
                                
     });
+  
+  
+    $('#edit-button').click(function(e){
+ 		if (confirm('Are you sure you want to update this information?')) {          
+          $('#interests').html($('#interest').val())
+          $('#skill').html($('#skills').val())
+          $('#location').html($('#locations').val())
+          $('#email').html($('#hone').val()) 
+      	  $('#tel').html($('#mail').val());
+	      $('#edit-form').modal('hide');
+          
+        }
+        });
     
 });
