4efc7e121880200ede2867061e26d5c3266e04fe
Ray Hua Wu <d684n@mit.edu>
2015-05-04 05:33:41 -0400

    google maps iframe added (and also borkèd)

--- contents/Event/result.js
+++ contents/Event/result.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-	$('#navbar').load('../partials/navbar.html', function() {
-		$('#navbar-brand').attr('href', '../voluntari.html');
-		$('#navbar-home').attr('href', '../voluntari.html');
-      	$('#navbar-prof').attr('href', '../profile/profile.html');      
-	});
-
-	$('#submitButton').click(function(){
-    	console.log('test');
+    $('#navbar').load('../partials/navbar.html', function() {
+	$('#navbar-brand').attr('href', '../voluntari.html');
+	$('#navbar-home').attr('href', '../voluntari.html');
+    	$('#navbar-prof').attr('href', '../profile/profile.html');      
+    });
+    $('#submitButton').click(function(){
     });
 });
