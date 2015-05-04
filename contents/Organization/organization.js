8c96b1f46081d7e7c3537c09a286080c89c0cc63
Shi-Ke Xue <shikexue@mit.edu>
2015-05-04 06:18:31 -0400

    separate partials for tabs and add event form, so that the format for each is clearer when adding new events

--- contents/Organization/organization.js
+++ contents/Organization/organization.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
       	$('#navbar-logout').attr('href', '../homepage.html');
     });
 
-	$('#datetimepicker').datetimepicker();
+    $('#events-tab').load('events.html');
+    $('#info-tab').load('info.html');
+
+    $('#add-form').load('add.html', function() {
+    	$('#datetimepicker').datetimepicker();
+    });
 
     var mapOptions = {zoom: 8, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};    
     var map = new google.maps.Map(mapDiv, mapOptions);
